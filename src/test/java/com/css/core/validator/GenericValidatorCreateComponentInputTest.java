c7c8b2368eb27e882052b2bbbe38aca6de41dae4
andre barreto <alab.home@gmail.com>
2023-09-08 17:31:11 -0300

    Component input hardware test

--- src/test/java/com/css/core/validator/GenericValidatorCreateComponentInputTest.java
+++ src/test/java/com/css/core/validator/GenericValidatorCreateComponentInputTest.java
@@ -2,7 +2,9 @@ package com.css.core.validator;
 
 import com.css.core.component.domain.ComponentTypeEnum;
 import com.css.core.component.domain.ManufacturerEnum;
+import com.css.core.component.domain.hardware.SocketTypeEnum;
 import com.css.core.component.usecase.createcomponent.input.CreateComponentInput;
+import com.css.core.component.usecase.createcomponent.input.MainBoardInput;
 import com.css.core.component.usecase.createcomponent.input.ProcessorInput;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,7 +20,7 @@ public class GenericValidatorCreateComponentInputTest {
     private final GenericValidator<CreateComponentInput> validator = new GenericValidator<>();
 
     @Test
-    @DisplayName("Validate: given a processor with invalid processor must return validation messages")
+    @DisplayName("Validate: given a processor component with a processor with zero and null fields must return validation messages")
     void validate_invalidProcessor_mustReturnValidationMessages(){
         final ProcessorInput processor = new ProcessorInput(
                 null,
@@ -46,5 +48,34 @@ public class GenericValidatorCreateComponentInputTest {
         assertTrue(constrains.contains("Invalid threads. Must be 1 or greater."));
         assertTrue(constrains.contains("Invalid cores. Must be 1 or greater."));
         assertTrue(constrains.contains("Energy consumption must be 10 or greater."));
+    }
+
+    @Test
+    @DisplayName("Validate: given a processor component with a diferent hardware must return validation messages")
+    void validate_invalidComponentType_mustReturnValidationMessages(){
+        final MainBoardInput mainBoard = new MainBoardInput(
+                SocketTypeEnum.LGA,
+                false,
+                false,
+                2,
+                50
+        );
+        final CreateComponentInput input = new CreateComponentInput(
+                "AMD Ryzen 5 5600G",
+                ManufacturerEnum.AMD,
+                ComponentTypeEnum.PROCESSOR,
+                new BigDecimal("849.00"),
+                40,
+                null,
+                mainBoard,
+                null,
+                null
+        );
+
+        final List<String> constrains = validator.validate(input);
+
+        assertEquals(2, constrains.size());
+        assertTrue(constrains.contains("Component type does not match component hardware"));
+        assertTrue(constrains.contains("Invalid slots. Must between 4 and 12."));
     }
 }
